4f421983accbfc468d5f131b8751a54652f0b506
yusei <yuseitbn@gmail.com>
2023-04-22 23:57:59 +0900

    controller途中

--- src/main/java/com/example/todolist/controller/TodolistController.java
+++ src/main/java/com/example/todolist/controller/TodolistController.java
@@ -1,5 +1,14 @@
 package com.example.todolist.controller;
 
+import org.springframework.stereotype.Controller;
+
+import com.example.todolist.repository.Todorepository;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstractor
 public class TodolistController {
+    private final Todorepository todorepository;
     
 }
--- src/main/java/com/example/todolist/repository/Todorepository.java
+++ src/main/java/com/example/todolist/repository/Todorepository.java
@@ -1,5 +1,10 @@
 package com.example.todolist.repository;
 
-public class Todorepository {
-    
+import com.example.todolist.entity.Todo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Todorepository extends JpaRepository<Todo,Integer>{
 }
