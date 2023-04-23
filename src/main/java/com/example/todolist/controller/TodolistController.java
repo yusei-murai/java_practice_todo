6d3e91dfc4c93e33284eeccdb76a87b1ac8101b5
yusei <yuseitbn@gmail.com>
2023-04-23 19:03:07 +0900

    controller

--- src/main/java/com/example/todolist/controller/TodolistController.java
+++ src/main/java/com/example/todolist/controller/TodolistController.java
@@ -1,14 +1,27 @@
 package com.example.todolist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.todolist.repository.Todorepository;
+import com.example.todolist.entity.Todo;
 
 import lombok.AllArgsConstructor;
+import java.util.List;
+
 
 @Controller
-@AllArgsConstractor
+@AllArgsConstructor
 public class TodolistController {
     private final Todorepository todorepository;
-    
+
+    @GetMapping("/todo")
+    private ModelAndView showTodoList(ModelAndView mv){
+        mv.setViewName("todoList");
+        List<Todo> todoList = todorepository.findAll();
+        mv.addObject("todoList", todoList);
+
+        return mv;
+    }
 }
