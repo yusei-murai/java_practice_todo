deece88c4a7c73b9df0cec617e620430c5b744a4
yusei <yuseitbn@gmail.com>
2023-04-24 17:18:28 +0900

    fix

--- src/main/java/com/example/todolist/controller/TodolistController.java
+++ src/main/java/com/example/todolist/controller/TodolistController.java
@@ -1,6 +1,7 @@
 package com.example.todolist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
